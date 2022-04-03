f35c7ab178ac5f64e826eb5b70217d74fa736a82
Mansour Marvasti <mansour.marvasti@outlook.com>
2022-04-03 17:05:06 +0430

    add SerialCommunication

--- UWP.Communications/ICommunication.cs
+++ UWP.Communications/ICommunication.cs
@@ -4,8 +4,7 @@ namespace Communications
 {
     public interface ICommunication
     {
-        //delegate void MessageReceivedHandler(object sender, string message);
-        //event MessageReceivedHandler MessageReceived;
+        
         bool IsConnected { get; }
         bool Connect();
         void Write(string data);
